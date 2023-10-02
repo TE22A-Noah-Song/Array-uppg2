d4af7ff5ec93fbd2eaf015673b34f1c266c66aa3
Noah Söng <noah.song@elev.ga.ntig.se>
2023-10-02 09:32:52 +0200

    gjort klart

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         int fält[]={1, 2, 3, 4};
         System.out.println(fält[0]+" "+ fält[1]+" "+fält[2]+" "+fält[3]);
-        
+
         int temp1=fält[0];
         int temp2=fält[1];
 
@@ -12,5 +13,30 @@ public class App {
         fält[3]=temp1;
 
         System.out.println(fält[0]+" "+ fält[1]+" "+fält[2]+" "+fält[3]);
+
+        String fält2[]={"Hej","på","dig"};
+        Scanner tangentbord=new Scanner(System.in);
+        String gissning;
+        String rätt_svar=fält2[1];
+        
+        System.out.print("Hej på ___. Gissa ordet: ");
+    gissning = tangentbord.nextLine();
+
+    System.out.println(gissning+"="+fält2[2]+":"+fält2[2].equals(gissning));
+
+    // if (fält2[0].equals(gissning)) 
+    // {
+    //     System.out.println("Rätt");
+    // }
+    // else if( fält2[1].equals(gissning) )
+    // {
+    //     System.out.println("Fel");    
+    // }
+    // else if( fält2[2].equals(gissning) )
+    // {
+
+    
+
+
     }
 }
